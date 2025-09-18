06fa352cbacc46213415a277a8fa26d8856572f2
Wei Zhu <rzzzw88@gmail.com>
2025-09-18 16:28:02 -0700

    built a GraphQL API,can now run queries and mutations against an in-memory data object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,72 @@
 // index.js
 // This is the main entry point of our application
+
 const express = require('express')
 
-const app = express();
+// const Apollo = require('apollo-server-express');
+// const ApolloServer = Apollo.ApolloServer;
+// const gql = Apollo.gql;
+const { ApolloServer, gql } = require('apollo-server-express');
+
+// Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello Web Server!!:D'));
+let notes = [
+    {id:'1', content:'This is a note', author: 'Adam Scott'},
+    {id:'2', content:'This is another note', author: 'Wei'},
+    {id:'3', content:'oh hey look, another note!', author: 'Will'}
+];
 
-app.listen(port, () => 
-    console.log('Server running at http://localhost:${port}')
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+    type Note {
+        id: ID!
+        content: String!
+        author: String!
+    }
+    type Query {
+        hello: String
+        notes: [Note!]!
+        note(id: ID!): Note!
+    }
+    type Mutation {
+        newNote(content: String!): Note!
+    }
+`;
+
+// Provide resolver functions for our schema fields
+const resolvers = {
+    Query: {
+        hello:() => 'Hello world!',
+        notes:() => notes,
+        note:(parent, args) => {
+            return notes.find(note => note.id === args.id);
+        }
+    },
+    Mutation: {
+        newNote: (parent, args) => {
+            let noteValue = {
+                id: String(notes.length + 1),
+                content: args.content,
+                author: 'Adam Scott'
+            };
+            notes.push(noteValue);
+            return noteValue;
+        }
+    }   
+};
+
+const app = express();
+
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Apply the Apollo GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
+
+app.listen({ port }, () =>
+    console.log(
+        `GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+    )
 );
 
